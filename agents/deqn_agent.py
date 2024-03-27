eb3746c4d3fd60b51bc08d784a97b03e8c04c196
Herman Vestvik Thieme <hermanvest@gmail.com>
2024-03-27 10:42:04 +0100

    refactored to make scaling of carbon reservoirs only when feeding input to the neural network

--- agents/deqn_agent.py
+++ agents/deqn_agent.py
@@ -11,14 +11,14 @@ class DEQN_agent:
     def get_actions(self, state: tf.Tensor) -> tf.Tensor:
         """Returns actions for given state as per current policy."""
 
-        return self.policy_network(self.preprocess_time(state))
+        return self.policy_network(self.preprocess(state))
 
-    def preprocess_time(
+    def preprocess(
         self,
         state: tf.Tensor,
         varsigma: tf.Tensor = tf.constant(0.02, dtype=tf.float32),
     ) -> tf.Tensor:
-        """Applies time transformation as in Traeger (2014) and a min-max scaler to the carbon reservoirs.
+        """Applies time transformation as in Traeger (2014) and downscales carbon reservoirs from GtCO2 to 1000GtCO2.
 
         Time scaled as: hat(t) = -exp(-varsigma t)
 
@@ -31,6 +31,12 @@ class DEQN_agent:
         # Time located at index 6, where we select index 6 to, but not including 7.
         t = state[..., 6:7]
         hat_t = -tf.exp(-varsigma * t) + 1
-        preprocessed_state = tf.concat([state[..., :6], hat_t, state[..., 7:]], axis=-1)
+
+        m = state[..., 1:4]
+        m_scaled = m / 1000
+
+        preprocessed_state = tf.concat(
+            [state[..., :1], m_scaled, state[..., 4:6], hat_t], axis=-1
+        )
 
         return preprocessed_state
